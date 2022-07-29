9407f7bfe55f65c23594ac11c625027f71523e2b
kalliehuynh <kallie1@ualberta.ca>
2022-07-29 15:39:44 -0600

    Finished React exercise

---
+++ 1-react/src/ components/Content.js
@@ -0,0 +1,19 @@
+const Part = (props) => {
+    const { name, exercises } = props
+    return (
+        <p>{name} {exercises}</p>
+    )
+}
+
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+    return (
+        <div>
+            <Part name={part1} exercises={exercises1} />
+            <Part name={part2} exercises={exercises2} />
+            <Part name={part3} exercises={exercises3} />
+        </div>
+    )
+}
+
+export default Content
