683180500ad6ca469870f7bcf6c470c7de16da8e
kalliehuynh <kallie1@ualberta.ca>
2022-08-01 19:57:20 -0600

    organizing files into folders

---
+++ 1-intro-to-react/course_summary/src/ components/Content.js
@@ -0,0 +1,21 @@
+const Part = ({ part }) => {
+    const {name, exercises} = part
+
+    return (
+        <p>{name} {exercises}</p>
+    )
+}
+
+const Content = ({ parts }) => {
+    const [ part1, part2, part3 ] = parts  
+      
+    return (
+        <div>
+            <Part part={part1} />
+            <Part part={part2} />
+            <Part part={part3} />
+        </div>
+    )
+}
+
+export default Content
