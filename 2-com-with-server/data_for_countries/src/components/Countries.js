4078f003580ed6df803322452387d1fa36007464
kalliehuynh <kallie1@ualberta.ca>
2022-08-05 21:50:48 -0600

    FINALLY added show buttons to country list, api key is not working whyyyyy

--- 2-com-with-server/data_for_countries/src/components/Countries.js
+++ 2-com-with-server/data_for_countries/src/components/Countries.js
@@ -1,39 +1,76 @@
-const DisplayCountries = ({ countryData, newFilter }) => {
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Weather = ({country}) => {
+    console.log('country', country)
+    const api_key = process.env.REACT_APP_API_KEY
+    console.log('api_key', api_key)
+    const [lat, lng] = country.latlng
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}`)
+            .then(response =>
+                console.log('response', response)
+            )
+    })
+}
+
+const DisplayCountries = ({ countryData, newFilter }) => {  
+    const [viewCountry, setViewCountry] = useState({})
     const filteredCountries = countryData.filter(country => 
         country.name.common.toLowerCase().includes(newFilter.toLowerCase()) 
     )
-    if (newFilter === '') {
-        return
-    }
-    if (filteredCountries.length === 1) {
-        const country = filteredCountries[0]
+    
+    const CountryPage = () => {       
         return (
             <>
-                <h2>{country.name.common}</h2>
+                <h2>{viewCountry.name.common}</h2>
                 <div>
-                    <p>capital {country.capital}</p>
-                    <p>area {country.area}</p>
+                    <p>capital {viewCountry.capital}</p>
+                    <p>area {viewCountry.area}</p>
                 </div>
                 <h3>languages:</h3>
                 <ul>
-                    {Object.values(country.languages)
+                    {Object.values(viewCountry.languages)
                         .map(language => 
                             <li key={language}>{language}</li>
                         )}
                 </ul>
-                <img src={country.flags.png} />
+                <img src={viewCountry.flags.png} />
+                <Weather country={viewCountry} />
             </>
-        
         )
+    }
+
+    if (Object.keys(viewCountry).length !== 0) {
+        return CountryPage(viewCountry)
+    }
+
+    const handleClick = (country) => {
+        setViewCountry(country)
+    }
+
+    if (newFilter === '') {
+        return
+    } 
+
+    if (filteredCountries.length > 10) {
+        return <p>Too many matches, specify another filter</p>
     }
     if (filteredCountries.length <= 10) {
         return filteredCountries.map(country =>
-            <p key={country.name.common} >{country.name.common}</p>
+            <div key={country.name.common}>
+                {country.name.common} 
+                <button onClick={() => {
+                    handleClick(country)}}> 
+                    show
+                </button>
+            </div> 
         )
     }
-    return <p>Too many matches, specify another filter</p>
-
-    
+    if (filteredCountries.length === 1) {
+        setViewCountry(filteredCountries[0])
+    }
 }
 
 export default DisplayCountries
